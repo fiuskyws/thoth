10e974e1d237ecdd370f4d211013f048e4cb34ea
Rafael Breno <rafael_breno@outlook.com>
2023-04-18 19:48:03 -0300

    feat: Add Manager

---
+++ src/manager/manager.go
@@ -0,0 +1,47 @@
+package manager
+
+import (
+	"fmt"
+
+	"github.com/fiuskyws/thoth/src/storage"
+)
+
+type (
+	Manager struct {
+		dbs map[string]storage.API
+	}
+)
+
+func NewManager() *Manager {
+	return &Manager{
+		dbs: map[string]storage.API{},
+	}
+}
+
+func (m *Manager) CreateTable(name string) error {
+	if _, ok := m.dbs[name]; ok {
+		return fmt.Errorf("database '%s' already exists", name)
+	}
+
+	m.dbs[name] = storage.NewMapDB()
+
+	return nil
+}
+
+func (m *Manager) Set(dbName, key, value string) error {
+	db, ok := m.dbs[dbName]
+	if !ok {
+		return fmt.Errorf("database '%s' does not exists", dbName)
+	}
+
+	return db.Set(key, value)
+}
+
+func (m *Manager) Get(dbName, key string) (string, error) {
+	db, ok := m.dbs[dbName]
+	if !ok {
+		return "", fmt.Errorf("database '%s' does not exists", dbName)
+	}
+
+	return db.Get(key)
+}
