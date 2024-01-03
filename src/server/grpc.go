9583194760ee0f11baa596182e327fdca678dfb2
Rafael Breno <rafael_breno@outlook.com>
2024-01-03 14:53:54 -0300

    feat: implement grpc server

---
+++ src/server/grpc.go
@@ -0,0 +1,66 @@
+package server
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/fiuskyws/thoth/src/manager"
+	"github.com/fiuskyws/thoth/src/proto"
+	"github.com/fiuskyws/thoth/src/repository"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+type (
+	// GRPC stores all info/data necessary to establish a
+	// gRPC server.
+	GRPC struct {
+		listener net.Listener
+		mgr      *manager.Manager
+		repo     *repository.GRPCRepo
+		srv      *grpc.Server
+		opts     []grpc.ServerOption
+	}
+)
+
+// NewGRPC returns a new GRPC connector.
+func NewGRPC(mgr *manager.Manager) *GRPC {
+	var opts []grpc.ServerOption
+
+	srv := grpc.NewServer(opts...)
+
+	repo := repository.NewGRPCRepo(mgr)
+
+	return &GRPC{
+		mgr:  mgr,
+		opts: opts,
+		repo: repo,
+		srv:  srv,
+	}
+}
+
+// Start - starts the gRPC server.
+func (g *GRPC) Start(port uint) error {
+	var err error
+	g.listener, err = net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", port))
+	if err != nil {
+		zap.L().Error(err.Error())
+		return err
+	}
+	proto.RegisterThothServer(g.srv, proto.ThothServer(g.repo))
+	if err := g.srv.Serve(g.listener); err != nil {
+		zap.L().Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+// Close - closes the gRPC server.
+func (g *GRPC) Close() error {
+	g.srv.GracefulStop()
+	if err := g.listener.Close(); err != nil {
+		zap.L().Error(err.Error())
+		return err
+	}
+	return nil
+}
